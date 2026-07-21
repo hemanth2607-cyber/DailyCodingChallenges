dd97962010358040e3d366d0d797be26a8e452e6
Hemanth <hemanthg2607@gmail.com>
2026-07-21 23:17:59 +0530

    Day 01/AddTwoIntegers.java

---
+++ Day 01/AddTwoIntegers.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class AddTwoIntegers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the first integer: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Enter the second integer: ");
+        int num2 = scanner.nextInt();
+
+        int sum = num1 + num2;
+        System.out.println(sum);
+        scanner.close();
+    }
+}
